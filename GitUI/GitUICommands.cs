ab21602e6c61e3da49e288805384c20c206635a8
Serhan Apaydın <zerhan@gmail.com>
2019-02-17 02:28:04 +0300

    Make commit window non-modal

--- GitUI/GitUICommands.cs
+++ GitUI/GitUICommands.cs
@@ -563,7 +563,18 @@ namespace GitUI
                 return true;
             }
 
-            return DoActionOnRepo(owner, Action, changesRepo: false, preEvent: PreCommit, postEvent: PostCommit);
+            var ehe = false;
+
+            if (ehe)
+            {
+                return DoActionOnRepo(owner, Action, changesRepo: false, preEvent: PreCommit, postEvent: PostCommit);
+            }
+
+            var form2 = new FormCommit(this) { ShowInTaskbar = true };
+
+            form2.Show();
+
+            return true;
         }
 
         public bool StartInitializeDialog(IWin32Window owner = null, string dir = null, EventHandler<GitModuleEventArgs> gitModuleChanged = null)
