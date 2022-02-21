e82103dce817060abcd1f17abed95e079818ab93
Serhan Apaydın <zerhan@gmail.com>
2022-02-21 20:57:09 +0300

    Make letter "G" of "GitExt" an accelerator

--- src/native/GitExtensionsShellEx/GitExtensionsShellEx.cpp
+++ src/native/GitExtensionsShellEx/GitExtensionsShellEx.cpp
@@ -670,7 +670,7 @@ UINT CGitExtensionsShellEx::AddMenuItem(HMENU hMenu, LPTSTR text, int resource, 
         mii.dwTypeData = text;
     else
     {
-        textEx = std::wstring(L"GitExt ") + text;
+        textEx = std::wstring(L"&GitExt ") + text;
         mii.dwTypeData = &textEx[0];
     }
 
