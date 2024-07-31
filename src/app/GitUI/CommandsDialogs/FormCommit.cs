d5eb1b15ecebaa16c70c4b8b7296e89df19dc92c
Serhan Apaydın <zerhan@gmail.com>
2024-07-31 07:33:14 +0300

    Do not fire RepoChangedNotifier.Notify() on commit

--- src/app/GitUI/CommandsDialogs/FormCommit.cs
+++ src/app/GitUI/CommandsDialogs/FormCommit.cs
@@ -1411,8 +1411,6 @@ namespace GitUI.CommandsDialogs
 
                     success = FormProcess.ShowDialog(this, UICommands, arguments: commitCmd, Module.WorkingDir, input: null, useDialogSettings: true);
 
-                    UICommands.RepoChangedNotifier.Notify();
-
                     if (!success)
                     {
                         return;
