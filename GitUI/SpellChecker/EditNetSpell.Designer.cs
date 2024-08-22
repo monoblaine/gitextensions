4670f1e5d0229749b28b01fc58114abbf81047d6
Serhan Apaydın <zerhan@gmail.com>
2024-08-22 20:14:32 +0300

    Use AppSettings.CommitFont in commit message textbox

--- GitUI/SpellChecker/EditNetSpell.Designer.cs
+++ GitUI/SpellChecker/EditNetSpell.Designer.cs
@@ -58,6 +58,7 @@
             this.TextBox.Size = new System.Drawing.Size(386, 336);
             this.TextBox.TabIndex = 1;
             this.TextBox.Text = "";
+            this.TextBox.Font = GitCommands.AppSettings.CommitFont;
             this.TextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyDown);
             this.TextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
             this.TextBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyUp);
