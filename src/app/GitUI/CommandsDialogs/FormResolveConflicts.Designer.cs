23f656d56682e5b4eeec1b0bd78faa06a403b346
Serhan Apaydın <zerhan@gmail.com>
2025-10-17 17:03:00 +0300

    Replace all "Show in folder"s with "Show &in folder"

--- src/app/GitUI/CommandsDialogs/FormResolveConflicts.Designer.cs
+++ src/app/GitUI/CommandsDialogs/FormResolveConflicts.Designer.cs
@@ -280,7 +280,7 @@ partial class FormResolveConflicts
         openFolderToolStripMenuItem.Image = Properties.Images.BrowseFileExplorer;
         openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
         openFolderToolStripMenuItem.Size = new Size(195, 22);
-        openFolderToolStripMenuItem.Text = "Show in folder";
+        openFolderToolStripMenuItem.Text = "Show &in folder";
         openFolderToolStripMenuItem.Click += openFolderToolStripMenuItem_Click;
         // 
         // toolStripSeparator4
