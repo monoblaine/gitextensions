f5c4aacfc09c76771c1de980e0d4d25fe04df79d
Serhan Apaydın <zerhan@gmail.com>
2025-11-26 10:24:02 +0300

    [Form Commit Window] Make "focus" hotkeys jump somewhere else if the target component is already focused

--- src/app/GitUI/CommandsDialogs/FormCommit.cs
+++ src/app/GitUI/CommandsDialogs/FormCommit.cs
@@ -764,25 +764,57 @@ namespace GitUI.CommandsDialogs
 
         private bool FocusStagedFiles()
         {
-            Staged.Focus();
+            if (Staged.Focused)
+            {
+                SelectedDiff.Focus();
+            }
+            else
+            {
+                Staged.Focus();
+            }
+
             return true;
         }
 
         private bool FocusUnstagedFiles()
         {
-            Unstaged.Focus();
+            if (Unstaged.Focused)
+            {
+                SelectedDiff.Focus();
+            }
+            else
+            {
+                Unstaged.Focus();
+            }
+
             return true;
         }
 
         private bool FocusSelectedDiff()
         {
-            SelectedDiff.Focus();
+            if (SelectedDiff.ContainsFocus)
+            {
+                Message.Focus();
+            }
+            else
+            {
+                SelectedDiff.Focus();
+            }
+
             return true;
         }
 
         private bool FocusCommitMessage()
         {
-            Message.Focus();
+            if (Message.ContainsFocus)
+            {
+                SelectedDiff.Focus();
+            }
+            else
+            {
+                Message.Focus();
+            }
+
             return true;
         }
 
